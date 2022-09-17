c92569c8a7ed543a5658b5cc315ccd7fc60969c3
Leo <leo.maedje@gmail.com>
2022-09-17 21:59:22 +0200

    Added server URL

--- src/utils/databaseURL.ts
+++ src/utils/databaseURL.ts
@@ -1,4 +1,4 @@
 export const baseURL =
   process.env.NODE_ENV === "production"
-    ? "https://your-project.herokuapp.com"
+    ? "https://lm-wordle-platform.herokuapp.com/"
     : "http://localhost:4000";
