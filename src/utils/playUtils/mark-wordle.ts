8cfbe6ad32ca47ffdcf35063be1266bc704b786e
Leo <leo.maedje@gmail.com>
2022-10-23 14:44:03 +0100

    Fixed removing all yellows bug

--- src/utils/playUtils/mark-wordle.ts
+++ src/utils/playUtils/mark-wordle.ts
@@ -26,18 +26,19 @@ function markIgnoringRepetition(
   guess: string,
   hiddenTarget: string
 ): MarkedGuess {
-  const result: MarkedGuess = { 0: { letter: guess[0], colour: grey } };
+  const result: MarkedGuess = {};
   for (let i = 0; i < 5; i++) {
     const guessLetter: string = guess[i];
     const targetLetter: string = hiddenTarget[i];
-    if (guessLetter === targetLetter)
+    if (guessLetter === targetLetter) {
       result[i] = { letter: guessLetter, colour: green };
-    else if (hiddenTarget.includes(guessLetter)) {
+    } else if (hiddenTarget.includes(guessLetter)) {
       result[i] = { letter: guessLetter, colour: yellow };
     } else {
       result[i] = { letter: guessLetter, colour: grey };
     }
   }
+  console.log(result);
   return result;
 }
 
@@ -47,9 +48,11 @@ function removeExtraYellows(
   hiddenTarget: string
 ): MarkedGuess {
   const modifiedResult: MarkedGuess = resultSoFar;
+  console.log(modifiedResult);
   // remove excess yellows of the duplicate letter from markedResult, starting from the back, replace with grey
   for (let i = 4; i >= 0; i--) {
     if (isDuplicateYellow(modifiedResult, guess, hiddenTarget, i)) {
+      console.log(modifiedResult);
       modifiedResult[i].colour = grey;
     }
   }
@@ -63,19 +66,48 @@ function isDuplicateYellow(
   i: number
 ): boolean {
   const guessLetter = guess[i];
+  console.log(resultSoFar[i].colour);
   if (!(resultSoFar[i].colour === yellow)) {
     return false;
   }
-  const letterInGuess = countLetter(guessLetter, guess);
-  const letterInTarget = countLetter(guessLetter, hiddenTarget);
-  const excessYellows = letterInGuess - letterInTarget;
+  const yellowLettersInGuess = countYellowLetterInGuess(
+    guessLetter,
+    resultSoFar
+  );
+  const lettersInTarget = countLetterInTarget(guessLetter, hiddenTarget);
+  console.log(
+    "i:",
+    i,
+    "yellowLettersInGuess:",
+    yellowLettersInGuess,
+    "lettersInTarget:",
+    lettersInTarget
+  );
+  const excessYellows = yellowLettersInGuess - lettersInTarget;
   if (excessYellows > 0) {
     return true;
   }
   return false;
 }
 
-function countLetter(letter: string, searchWord: string): number {
+function countYellowLetterInGuess(
+  guessLetter: string,
+  resultSoFar: MarkedGuess
+): number {
+  let letterCount = 0;
+  for (let i = 0; i < 5; i++) {
+    const currentLetter = resultSoFar[i];
+    if (
+      currentLetter.letter === guessLetter &&
+      currentLetter.colour === yellow
+    ) {
+      letterCount++;
+    }
+  }
+  return letterCount;
+}
+
+function countLetterInTarget(letter: string, searchWord: string): number {
   const regexp = new RegExp(letter, "g");
   let letterCount = 0;
   const letterArray: string[] | null = searchWord.match(regexp);
